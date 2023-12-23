4e29db4d63bfcad76866a3186ecf9a032c7ddc75
Jonas Slaunwhite <jonas.slaunwhite@gmail.com>
2023-12-23 17:09:41 -0400

    add asteroids and collision detection

--- cosmic_crust_cruiser.py
+++ cosmic_crust_cruiser.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import numpy as np
 
 # Initialize Pygame
 pygame.init()
@@ -14,7 +15,30 @@ pygame.display.set_caption("Cosmic Crust Cruiser")
 
 # Load spaceship image
 ship_img = pygame.image.load("spaceship.png")
-ship_rect = ship_img.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+ship_rect = ship_img.get_rect(center=(SCREEN_WIDTH/4, SCREEN_HEIGHT/4))
+
+# Load asteroid image
+asteroid_img = pygame.image.load("asteroid.png")
+asteroids = []
+
+# Function to create a new asteroid
+def create_asteroid():
+    new_asteroid = asteroid_img.get_rect(topleft=(np.random.randint(800, 1600), np.random.randint(0, 600)))
+    asteroids.append(new_asteroid)
+
+# Function to move and draw asteroids
+def move_asteroids():
+    for asteroid in asteroids:
+        asteroid.x -= 5  # Move asteroid to the left
+        if asteroid.x < -50:  # If asteroid is off-screen, remove it
+            asteroids.remove(asteroid)
+        screen.blit(asteroid_img, asteroid)
+
+def detect_collisions():
+    for asteroid in asteroids:
+        if ship_rect.colliderect(asteroid):  # Check if the spaceship rectangle collides with an asteroid
+            return True
+    return False
 
 # Game loop
 running = True
@@ -34,6 +58,15 @@ while running:
         ship_rect.y -= SHIP_SPEED
     if keys[pygame.K_DOWN]:
         ship_rect.y += SHIP_SPEED
+
+    # Obstacle: Create a new asteroid every so often
+    if np.random.randint(1, 50) == 1:
+        create_asteroid()
+    move_asteroids()
+
+    if detect_collisions():
+        print("Collision detected! Game over.")
+        running = False
     
     # Drawing
     screen.fill((0,0,0)) # Fills the screen with black space
